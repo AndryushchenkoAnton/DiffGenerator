0c723392bae4745beeaf070ab112a09a8932becb
Антон Андрющенко <lazarenkoanton527@gmail.com>
2023-04-24 20:43:55 +0300

    fixes

--- src/formaters/plain.js
+++ src/formaters/plain.js
@@ -12,30 +12,32 @@ const makeFormattedValue = (value) => {
   return value;
 };
 
-const iterPlain = (valueOf, parents = []) => {
-  const key = getKey(valueOf);
-  const value = getValue(valueOf);
-  const [original, changed] = getChangedValues(valueOf);
-  const type = getType(valueOf);
-  const way = [...parents, key].join('.');
+const makeModule = (array, parents = []) => {
+  const iterPlain = (valueOf, parentsOf) => {
+    const key = getKey(valueOf);
+    const value = getValue(valueOf);
+    const [original, changed] = getChangedValues(valueOf);
+    const type = getType(valueOf);
+    const way = [...parentsOf, key].join('.');
 
-  switch (type) {
-    case 'children':
-      return makeModule(value, [...parents, key]);
-    case 'added':
-      return `Property '${way}' was added with value: ${makeFormattedValue(value)}`;
-    case 'deleted':
-      return `Property '${way}' was removed`;
+    switch (type) {
+      case 'children':
+        return makeModule(value, [...parentsOf, key]);
+      case 'added':
+        return `Property '${way}' was added with value: ${makeFormattedValue(value)}`;
+      case 'deleted':
+        return `Property '${way}' was removed`;
 
-    case 'changed':
-      return `Property '${way}' was updated. From ${makeFormattedValue(original)} to ${makeFormattedValue(changed)}`;
+      case 'changed':
+        return `Property '${way}' was updated. From ${makeFormattedValue(original)} to ${makeFormattedValue(changed)}`;
 
-    default:
-      return '';
-  }
+      default:
+        return '';
+    }
+  };
+
+  return array.filter((el) => getType(el) !== 'unchanged').map((el) => iterPlain(el, [...parents])).join('\n');
 };
-
-const makeModule = (array, parents = []) => array.filter((el) => getType(el) !== 'unchanged').map((el) => iterPlain(el, [...parents])).join('\n');
 
 const plain = (difference) => makeModule(difference);
 export default plain;
--- src/formaters/stylish.js
+++ src/formaters/stylish.js
@@ -6,52 +6,54 @@ import {
 const makeSpace = (spaceSymbol, count = 2) => spaceSymbol.repeat(count);
 
 const makeModule = (array, depth) => {
-  const final = array.map((el) => iter(el, depth));
-  final.unshift('{');
-  final.push(`${makeSpace(' ', depth * 2 - 4)}}`);
-  return final.join('\n');
-};
-const makeLine = (valueString, counter) => {
-  if (!_.isObject(valueString)) {
-    return valueString;
-  }
-  const res = Object
-    .entries(valueString)
-    .map(([key, val]) => `${makeSpace(' ', counter * 2)}${key}: ${iter(val, counter + 2)}`);
-  return [
-    '{',
-    res.join('\n'),
-    `${makeSpace(' ', counter * 2 - 4)}}`,
-  ].join('\n');
-};
-const iter = (valueOf, count) => {
-  if (!_.isObject(valueOf) || !Object.hasOwn(valueOf, 'type')) {
-    return makeLine(valueOf, count);
-  }
-  const type = getType(valueOf);
-  const key = getKey(valueOf);
+  const iter = (valueOf, count) => {
+    const makeLine = (valueString, counter) => {
+      if (!_.isObject(valueString)) {
+        return valueString;
+      }
+      const res = Object
+        .entries(valueString)
+        .map(([key, val]) => `${makeSpace(' ', counter * 2)}${key}: ${iter(val, counter + 2)}`);
+      return [
+        '{',
+        res.join('\n'),
+        `${makeSpace(' ', counter * 2 - 4)}}`,
+      ].join('\n');
+    };
 
-  if (type === 'children') {
-    const value = getValue(valueOf);
-    return `${makeSpace(' ', count * 2 - 2)}  ${key}: ${makeModule(value, count + 2)}`;
-  }
-  if (type === 'unchanged') {
-    const value = getValue(valueOf);
-    return `${makeSpace(' ', count * 2)}${key}: ${iter(value, count + 2)}`;
-  }
-  if (type === 'changed') {
-    const [original, change] = getChangedValues(valueOf);
-    const first = `${makeSpace(' ', count * 2 - 2)}- ${key}: ${iter(original, count + 2)}`;
-    const second = `${makeSpace(' ', count * 2 - 2)}+ ${key}: ${iter(change, count + 2)}`;
-    return [first, second].join('\n');
-  } if (type === 'added') {
-    const value = getValue(valueOf);
-    return `${makeSpace(' ', count * 2 - 2)}+ ${key}: ${iter(value, count + 2)}`;
-  } if (type === 'deleted') {
-    const value = getValue(valueOf);
-    return `${makeSpace(' ', count * 2 - 2)}- ${key}: ${iter(value, count + 2)}`;
-  }
-  return null;
+    if (!_.isObject(valueOf) || !Object.hasOwn(valueOf, 'type')) {
+      return makeLine(valueOf, count);
+    }
+    const type = getType(valueOf);
+    const key = getKey(valueOf);
+
+    if (type === 'children') {
+      const value = getValue(valueOf);
+      return `${makeSpace(' ', count * 2 - 2)}  ${key}: ${makeModule(value, count + 2)}`;
+    }
+    if (type === 'unchanged') {
+      const value = getValue(valueOf);
+      return `${makeSpace(' ', count * 2)}${key}: ${iter(value, count + 2)}`;
+    }
+    if (type === 'changed') {
+      const [original, change] = getChangedValues(valueOf);
+      const first = `${makeSpace(' ', count * 2 - 2)}- ${key}: ${iter(original, count + 2)}`;
+      const second = `${makeSpace(' ', count * 2 - 2)}+ ${key}: ${iter(change, count + 2)}`;
+      return [first, second].join('\n');
+    } if (type === 'added') {
+      const value = getValue(valueOf);
+      return `${makeSpace(' ', count * 2 - 2)}+ ${key}: ${iter(value, count + 2)}`;
+    } if (type === 'deleted') {
+      const value = getValue(valueOf);
+      return `${makeSpace(' ', count * 2 - 2)}- ${key}: ${iter(value, count + 2)}`;
+    }
+    return null;
+  };
+
+  const final = array.map((el) => iter(el, depth));
+  const res = ['{', ...final, `${makeSpace(' ', depth * 2 - 4)}}`];
+
+  return res.join('\n');
 };
 
 const stylish = (differences) => makeModule(differences, 2);
