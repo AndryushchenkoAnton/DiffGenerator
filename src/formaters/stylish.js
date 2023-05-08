99aa3d563e78531b117d27cb34e1c1a5f6dda29c
Антон Андрющенко <lazarenkoanton527@gmail.com>
2023-05-09 00:01:04 +0300

    fixes

--- src/formaters/stylish.js
+++ src/formaters/stylish.js
@@ -5,54 +5,31 @@ import {
 
 const makeSpace = (spaceSymbol, count = 2) => spaceSymbol.repeat(count);
 
-const makeDifferenceOfChildren = (array, space) => {
+const stylish = (differences, space = 2) => {
   const iter = (node, depth) => {
-    const makeLine = (currentNode, currentDepth) => {
-      if (!_.isObject(currentNode)) {
-        return currentNode;
-      }
-      const res = Object
-        .entries(currentNode)
-        .map(([key, val]) => `${makeSpace(' ', currentDepth * 2)}${key}: ${iter(val, currentDepth + 2)}`);
-      return [
-        '{',
-        res.join('\n'),
-        `${makeSpace(' ', currentDepth * 2 - 4)}}`,
-      ].join('\n');
-    };
-
-    if (!_.isObject(node) || !Object.hasOwn(node, 'type')) {
-      return makeLine(node, depth);
+    if (getType(node) === 'children') {
+      return `${makeSpace(' ', depth * 2 - 2)}  ${getKey(node)}: ${stylish(getValue(node), depth + 2)}`;
     }
-    const type = getType(node);
-    const key = getKey(node);
-    if (type === 'children') {
-      const valueOfNode = getValue(node);
-      return `${makeSpace(' ', depth * 2 - 2)}  ${key}: ${makeDifferenceOfChildren(valueOfNode, depth + 2)}`;
+    if (getType(node) === 'unchanged') {
+      return `${makeSpace(' ', depth * 2)}${getKey(node)}: ${iter(getValue(node), depth + 2)}`;
     }
-    if (type === 'unchanged') {
-      const valueOfNode = getValue(node);
-      return `${makeSpace(' ', depth * 2)}${key}: ${iter(valueOfNode, depth + 2)}`;
-    }
-    if (type === 'changed') {
-      const [original, change] = getChangedValues(node);
-      const first = `${makeSpace(' ', depth * 2 - 2)}- ${key}: ${iter(original, depth + 2)}`;
-      const second = `${makeSpace(' ', depth * 2 - 2)}+ ${key}: ${iter(change, depth + 2)}`;
+    if (getType(node) === 'changed') {
+      const [original, changed] = getChangedValues(node);
+      const first = `${makeSpace(' ', depth * 2 - 2)}- ${getKey(node)}: ${iter(original, depth + 2)}`;
+      const second = `${makeSpace(' ', depth * 2 - 2)}+ ${getKey(node)}: ${iter(changed, depth + 2)}`;
       return [first, second].join('\n');
-    } if (type === 'added') {
-      const valueOfNode = getValue(node);
-      return `${makeSpace(' ', depth * 2 - 2)}+ ${key}: ${iter(valueOfNode, depth + 2)}`;
-    } if (type === 'deleted') {
-      const valueOfNode = getValue(node);
-      return `${makeSpace(' ', depth * 2 - 2)}- ${key}: ${iter(valueOfNode, depth + 2)}`;
+    } if (getType(node) === 'added') {
+      return `${makeSpace(' ', depth * 2 - 2)}+ ${getKey(node)}: ${iter(getValue(node), depth + 2)}`;
+    } if (getType(node) === 'deleted') {
+      return `${makeSpace(' ', depth * 2 - 2)}- ${getKey(node)}: ${iter(getValue(node), depth + 2)}`;
     }
-    return null;
+    if (!_.isObject(node)) {
+      return node;
+    }
+    const res = Object.entries(node).map(([key, val]) => `${makeSpace(' ', depth * 2)}${key}: ${iter(val, depth + 2)}`);
+    return ['{', res.join('\n'), `${makeSpace(' ', depth * 2 - 4)}}`].join('\n');
   };
-  const final = array.map((el) => iter(el, space));
-  const res = ['{', ...final, `${makeSpace(' ', space * 2 - 4)}}`];
-  return res.join('\n');
+  return ['{', ...differences.map((el) => iter(el, space)), `${makeSpace(' ', space * 2 - 4)}}`].join('\n');
 };
-
-const stylish = (differences) => makeDifferenceOfChildren(differences, 2);
 
 export default stylish;
--- src/methods.js
+++ src/methods.js
@@ -1,4 +1,11 @@
-const getType = (object) => object.type;
+import _ from 'lodash';
+
+const getType = (object) => {
+  if (_.isObject(object)) {
+    return Object.hasOwn(object, 'type') ? object.type : false;
+  }
+  return null;
+};
 const getValue = (object) => object.value;
 const getKey = (object) => object.key;
 const getChangedValues = (object) => [object.value1, object.value2];
